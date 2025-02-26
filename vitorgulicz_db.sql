c0396931df059d728a70ecb98faf558e71bbcb9b
VitorGulicz <vdmgulicz@gmail.com>
2025-02-25 21:01:23 -0300

    sql

---
+++ vitorgulicz_db.sql
@@ -0,0 +1,9 @@
+create database vitorgulicz_db
+create table usuario(
+	idusuario int not null auto_increment,
+    nome text,
+    telefone text,
+    usuario text,
+    senha text,
+    primary key (idusuario)
+);
