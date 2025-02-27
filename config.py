22fc1673e454b4f32fd00003f5ec51f1befb0d6a
VitorGulicz <vdmgulicz@gmail.com>
2025-02-26 22:06:37 -0300

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 MYSQYL_HOST = 'localhost'
 MYSQYL_USER = 'root'
-MYSQYL_PASSWORD = 'root'
+MYSQYL_PASSWORD = ''
 MYSQYL_DATABASE = 'vitorgulicz_db'
 
 #para rodar o projeto não posso esquecer de rodar o cofigo abaixo 
